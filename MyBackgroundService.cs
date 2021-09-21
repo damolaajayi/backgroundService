8dab19a86acf2e8b0ba95a4061477359e35a33be
Adedamola Ajayi <adedamola.ajayi@titantrustbank.com>
2021-09-21 17:09:52 +0100

    first commit

---
+++ MyBackgroundService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BackgroundServiceTutorial
+{
+    public class MyBackgroundService : BackgroundService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger<MyBackgroundService> _logger;
+        public MyBackgroundService(IServiceProvider serviceProvider, ILogger<MyBackgroundService> logger)
+        {
+            _serviceProvider = serviceProvider;
+            _logger = logger;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                using (var scope = _serviceProvider.CreateScope())
+                {
+                    _logger.LogInformation("From MyBackgroundService : ExecuteAsync {dateTime}", DateTime.Now);
+                    var scopedService = scope.ServiceProvider.GetRequiredService<IScopedService>();
+                    scopedService.Write();
+                    await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+                }
+            }
+            
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("From MyBackgroundService : StopAsync {dateTime}", DateTime.Now);
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
---
+++ MyScopedService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackgroundServiceTutorial
+{
+    public class MyScopedService : IScopedService
+    {
+        private readonly ILogger<MyScopedService> _logger;
+        public MyScopedService(ILogger<MyScopedService> logger)
+        {
+            _logger = logger;
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public void Write()
+        {
+            _logger.LogInformation("MyScopedService {Id}", Id);
+        }
+
+    }
+
+    public interface IScopedService
+    {
+        void Write();
+    }
+}
